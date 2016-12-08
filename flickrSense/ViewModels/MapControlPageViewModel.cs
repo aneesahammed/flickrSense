4af680ce2a4ca34fb76599004a86d3c0e7b0db14
aneesahammed <aneesahammed@gmail.com>
2016-12-08 22:48:31 +0400

    #adds maps-control View and ViewModel #adds adaptive-gridview control

---
+++ flickrSense/ViewModels/MapControlPageViewModel.cs
@@ -0,0 +1,89 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using flickrSense.Common.Storage;
+using flickrSense.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using Template10.Services.NavigationService;
+using Windows.Devices.Geolocation;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace flickrSense.ViewModels
+{
+    public class MapControlPageViewModel: ViewModelBase
+    {
+        #region <-PrivateMembers->
+        private object _locker = new object();
+
+        #endregion
+
+        #region <-Properties->
+        //private Location _location = null;
+        //public Location Location { get; set; }
+        #endregion
+
+        #region <-Constructor->
+        public MapControlPageViewModel()
+        {
+
+        }
+        #endregion
+
+        #region <-PublicMethods->
+        private static int _testCounter = 1;
+        public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
+        {
+            try
+            {
+                if(parameter is Photo)
+                {
+                    var photo= parameter as Photo;
+
+                    // Specify a known location.
+                    BasicGeoposition snPosition = new BasicGeoposition() { Latitude = photo.Latitude, Longitude = photo.Longitude+ _testCounter++ };
+                    Geopoint snPoint = new Geopoint(snPosition);
+
+                    Messenger.Default.Send<MapInfo>(new MapInfo() { SnPoint=snPoint,Title=photo.Title});
+                }
+
+                await Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                AppLogs.WriteError("[OnNavigatedToAsync]", ex.ToString());
+            }
+        }
+
+        public override async Task OnNavigatedFromAsync(IDictionary<string, object> suspensionState, bool suspending)
+        {
+            try
+            {
+                if (suspending)
+                {
+
+                }
+                await Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                AppLogs.WriteError("[OnNavigatedFromAsync]", ex.ToString());
+            }
+        }
+
+        public override async Task OnNavigatingFromAsync(NavigatingEventArgs args)
+        {
+            try
+            {
+                args.Cancel = false;
+                await Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                AppLogs.WriteError("[OnNavigatingFromAsync]", ex.ToString());
+            }
+        }
+        #endregion
+    }
+}
---
+++ flickrSense/Views/MapControlPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using flickrSense.Common.Storage;
+using flickrSense.Models;
+using System;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
+
+namespace flickrSense.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MapControlPage : Page
+    {
+        public static readonly Geopoint SeattleGeopoint = new Geopoint(new BasicGeoposition() { Latitude = 47.604, Longitude = -122.329 });
+        RandomAccessStreamReference mapIconStreamReference;
+
+        public MapControlPage()
+        {
+            this.InitializeComponent();
+            mapIconStreamReference = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/MapPin.png"));
+
+            Messenger.Default.Register<MapInfo>(this,SetMapView);
+        }
+
+        private void SetMapView(MapInfo mapInfo)
+        {
+            try
+            {
+                // Create a MapIcon.
+                MapIcon mapIcon1 = new MapIcon();
+                mapIcon1.Location = mapInfo.SnPoint;
+                mapIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
+                mapIcon1.Title = mapInfo.Title;
+                mapIcon1.ZIndex = 0;
+
+                // Add the MapIcon to the map.
+                mapControl.MapElements.Add(mapIcon1);
+
+                // Center the map over the POI.
+                mapControl.Center = mapInfo.SnPoint;
+                mapControl.ZoomLevel = 14;
+            }
+            catch (Exception ex)
+            {
+                AppLogs.WriteError("[SetMapView]", ex.ToString());
+            }
+           
+        }
+    }
+}
